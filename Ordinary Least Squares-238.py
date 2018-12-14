881c9edf8cef25d68d1cd2dd3d5567aef38b6bf9
Nem kumar Kothari <32135057+nemkothari@users.noreply.github.com>
2018-12-14 10:05:21 +0530

    Add files via upload

---
+++ Ordinary Least Squares-238.py
@@ -0,0 +1,30 @@
+## 1. Introduction ##
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+data = pd.read_csv('AmesHousing.txt', delimiter="\t")
+train = data[0:1460]
+test = data[1460:]
+
+features = ['Wood Deck SF', 'Fireplaces', 'Full Bath', '1st Flr SF', 'Garage Area',
+       'Gr Liv Area', 'Overall Qual']
+
+X = train[features]
+y = train['SalePrice']
+
+first_term = np.linalg.inv(
+        np.dot(
+            np.transpose(X), 
+            X
+        )
+    )
+second_term = np.dot(
+        np.transpose(X),
+        y
+    )
+a = np.dot(first_term, second_term)
+print(a)
